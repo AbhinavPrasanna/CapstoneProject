225189a59bde1aef6e82b4821f201b51902db89a
Abhinav Prasanna <abhinavprasanna@Abhinavs-Air.attlocal.net>
2023-09-21 13:15:39 -0500

    Added Basic Navigation to React Application with Login and Sign up with basic input components and correct fonts. Additionally added Cinemania logo and routing to application

---
+++ film-app/src/Components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Stylesheets/Navbar.css'
+import './Stylesheets/Logos.css'
+import "@fontsource/black-han-sans"; 
+import "@fontsource/baloo-2";
+
+function Navbar() {
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <nav className="nav">
+       <a className="CinemaniaLogoLayout">
+       <span className="CinemaniaLogoTypography">CINEMANIA</span>
+      </a>
+      <a className='SignUpButtonLayout'>
+        <button className = "SignUpButtonStyle SignUpTextTypography"onClick={() => navigateTo("/signup")} type="button">
+          Sign Up
+        </button>
+      </a>
+      <a className='LoginTextLayout'>
+      <button className='LoginTextTypography TransparentButton' onClick={() => navigateTo("/login")} type="button">
+        Login
+      </button>
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
